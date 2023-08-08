e8d12dddf0f8c920694b3fa0bfb49da6d3154950
kaiquesilvadev <j.kaique1570@gmail.com>
2023-08-08 18:49:02 -0300

    ex5 finalizado 1.0

--- src/ex5enum/aplicacao.java
+++ src/ex5enum/aplicacao.java
@@ -1,16 +1,20 @@
 package ex5enum;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class aplicacao {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
 		/*Ler os dados de um trabalhador com N contratos (N fornecido pelo usuário). Depois, solicitar 
 		do usuário um mês e mostrar qual foi o salário do funcionário nesse mês, conforme exemplo 
 		(próxima página).*/
 		
 		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat stc = new SimpleDateFormat("dd/MM/yyyyy");
 
 		System.out.println("entre com o nome do departamento");
 		String departamento = sc.nextLine();
@@ -28,6 +32,22 @@ public class aplicacao {
 		Worker worker = new  Worker(nome , salario , WorKerLevel.valueOf(nivel) , new Department(departamento));
 		sc.close();
 
+		System.out.println("quantos contratos?");
+		int contratos = sc.nextInt();
+		
+		for(int i = 0 ; i < contratos ; i++) {
+			System.out.println("data do contrato "+i);
+			Date data = stc.parse(sc.next());
+			
+			System.out.println("valor por horas do contrato "+1);
+			double valor = sc.nextDouble();
+			
+			System.out.println("vquantas horas contrato "+1);
+			int horas = sc.nextInt();
+			
+			HourContract contract = new HourContract(data , valor , horas);
+			worker.addContract(contract);
+		}
 	}
 
 }
