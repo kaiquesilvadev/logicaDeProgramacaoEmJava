956be0c7d9cbaa055ce63e41d1fee4e51de4baae
kaiquesilvadev <j.kaique1570@gmail.com>
2023-08-11 19:39:10 -0300

    ex7 desafio proposto curso nelio 0.0

--- src/TrabalhandoComArquivos/ex1.java
+++ src/TrabalhandoComArquivos/ex1.java
@@ -1,9 +1,27 @@
 package TrabalhandoComArquivos;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class ex1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		File file = new File("C:\\temp\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
 
 	}
 
