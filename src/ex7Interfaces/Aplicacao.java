ff7a787d9bc19a0684425d2c2d52cd62536c61a8
kaiquesilvadev <j.kaique1570@gmail.com>
2023-08-14 19:22:56 -0300

    ex7 curso nelio

--- src/ex7Interfaces/Aplicacao.java
+++ src/ex7Interfaces/Aplicacao.java
@@ -1,5 +1,10 @@
 package ex7Interfaces;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Aplicacao {
 
 	public static void main(String[] args) {
@@ -18,6 +23,36 @@ public class Aplicacao {
 		sendo a primeira parcela a ser paga um mês após a data do contrato, a segunda parcela dois
 		meses após o contrato e assim por diante. Mostrar os dados das parcelas na tela.
 		Veja exemplo na próxima página.*/
+		
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		System.out.println("Entre os dados do contrato:");
+		System.out.print("Numero: ");
+		int number = sc.nextInt();
+		System.out.print("Data (dd/MM/yyyy): ");
+		LocalDate date = LocalDate.parse(sc.next(), fmt);
+		System.out.print("Valor do contrato: ");
+		double totalValue = sc.nextDouble();
+		
+		Contract obj = new Contract(number, date, totalValue);
+		
+		System.out.print("Entre com o numero de parcelas: ");
+		int n = sc.nextInt();
+		
+		ContractService contractService = new ContractService(new PaypalService());
+		
+		contractService.processContract(obj, n);
+		
+		System.out.println("Parcelas:");
+		for (Installment installment : obj.getInstallments()) {
+			System.out.println(installment);
+		}
+		
+		sc.close();
 	}
 
 }
