9e7ac354ce590f9589ee0e5c08f7418b7c9b402d
kaiquesilvadev <j.kaique1570@gmail.com>
2023-08-23 22:21:34 -0300

    ex pra treinar stream

---
+++ src/ex9Stream/Aplicacao.java
@@ -0,0 +1,60 @@
+package ex9Stream;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		/*
+		 * pegar os nome das pessoas com menos de 25 anos ordenados pelo nome
+		 */
+
+		List<Person> person = Person.personList();
+
+		System.out.println(person);
+
+		List<String> pessoas = person.stream().filter(x -> x.getIge() < 25)
+				.sorted(Comparator.comparing(Person::getName)).limit(3).map(x -> x.getName())
+				.collect(Collectors.toList());
+
+		System.out.println(pessoas);
+
+		/*Dado um array de números inteiros, crie um novo array contendo o quadrado dos números pares 
+		 *presentes no array original.*/
+		
+		System.out.println("ex2");
+		
+		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+       numbers.stream().filter(x -> x%2==0).map(x -> x + x).forEach(System.out::println);
+       
+       /*Dado um array de nomes, crie um novo array contendo os nomes em letras maiúsculas.*/
+       
+       System.out.println("ex3");
+
+       List<String> names = Arrays.asList("Alice", "Bob", "Charlie", "David", "Eva");
+
+       List<String> uppercaseNames = names.stream().map(x -> x.toString().toUpperCase()).collect(Collectors.toList());
+
+       System.out.println(uppercaseNames);
+       
+       /*Dado um array de números inteiros, crie um novo array contendo o quadrado dos números pares presentes no array original.*/
+       
+       System.out.println("ex4");
+       
+       List<List<Integer>> numberLists = Arrays.asList(
+               Arrays.asList(1, 2, 3),
+               Arrays.asList(3, 4, 5),
+               Arrays.asList(5, 6, 7)
+           );
+
+           List<Integer> uniqueNumbers = numberLists.stream().flatMap(List::stream).distinct().collect(Collectors.toList());
+           
+           System.out.println(uniqueNumbers);
+	}
+
+}
---
+++ src/ex9Stream/Person.java
@@ -0,0 +1,87 @@
+package ex9Stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class Person {
+	
+	private String name;
+
+	private Integer ige;
+	
+	private Double salary;
+
+	public Person() {}
+
+	public Person(String name, Integer ige, Double salary) {
+		this.name = name;
+		this.ige = ige;
+		this.salary = salary;
+	}
+	
+	public static List<Person> personList(){
+		List<Person> person = Arrays.asList(
+				new Person("kaique", 30 , 1500.00),
+				new Person("paulo", 15 , 1670.00),
+				new Person("lucas", 18 , 1500.00),
+				new Person("pedro", 50 , 1600.00),
+				new Person("maria", 20 , 2500.00),
+				new Person("betania", 35 , 2234.00),
+				new Person("carlos", 12 , 1507.00),
+				new Person("larissa", 25 , 1800.00),
+				new Person("talita", 15 , 1900.00),
+				new Person("maicon", 17 , 3500.00)
+				);
+		return person;
+	}
+
+	
+	public Integer getIge() {
+		return ige;
+	}
+
+	public void setIge(Integer ige) {
+		this.ige = ige;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", ige=" + ige + ", salary=" + salary + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(ige, name, salary);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return Objects.equals(ige, other.ige) && Objects.equals(name, other.name)
+				&& Objects.equals(salary, other.salary);
+	}
+
+}
