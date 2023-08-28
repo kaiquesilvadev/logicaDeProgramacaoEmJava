80151fd644dc13bc03f1e2491cc5160e1c4e1101
kaiquesilvadev <j.kaique1570@gmail.com>
2023-08-28 20:13:03 -0300

    aula youTube , consumer

---
+++ src/parametrizacaoComportamento/ConsumeTest.java
@@ -0,0 +1,18 @@
+package parametrizacaoComportamento;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumeTest {
+
+	public static void main(String[] args) {
+		List<String> list = List.of("kaique", "bruno", "joão");
+		
+		forEach(list,System.out::println);
+	}
+	public static <T>void forEach(List<T> list , Consumer<T> consumer){
+		for(T e : list) {
+			consumer.accept(e);
+		}
+	} 
+}
