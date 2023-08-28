d1df169f6c31ed9d23b3731cc524d12a07b01ed8
kaiquesilvadev <j.kaique1570@gmail.com>
2023-08-28 20:36:29 -0300

    aula youTube , function

---
+++ src/parametrizacaoComportamento/FunctionTest.java
@@ -0,0 +1,23 @@
+package parametrizacaoComportamento;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class FunctionTest {
+
+	public static void main(String[] args) {
+    List<String> list	= List.of("natsu", "allucard");
+    List<Integer> integers = map(list,s -> s.length());
+    System.out.println(integers);
+	}
+	
+	public static <T, R> List<R> map(List<T> list, Function<T, R> function){
+		List<R> result = new ArrayList<>();
+		for (T e : list) {
+			R r = function.apply(e);
+			result.add(r);
+		}
+		return result ;
+	}
+}
