aacd45b4fa61837d770bbf18a104a76d8eb800e5
kaiquesilvadev <j.kaique1570@gmail.com>
2023-08-28 19:57:45 -0300

    aula youTube , Predicate

---
+++ src/parametrizacaoComportamento/Car.java
@@ -0,0 +1,46 @@
+package parametrizacaoComportamento;
+
+public class Car {
+	
+	private String name = "polo";
+	private Integer year;
+	private String color;
+	
+	public Car() {
+	}
+
+	public Car(Integer year, String color) {
+		this.year = year;
+		this.color = color;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [name=" + name + ", year=" + year + ", color=" + color + "]";
+	}
+	
+}
---
+++ src/parametrizacaoComportamento/ComportamentoTest.java
@@ -0,0 +1,29 @@
+package parametrizacaoComportamento;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ComportamentoTest {
+	
+	private static List<Car> cars = List.of(new Car(2011 ,"green"),new Car(2018 ,"bleck"));
+
+	public static void main(String[] args) {
+		
+		List<Car> colorCar = filter(cars, x -> x.getColor().equals("green"));
+		System.out.println(colorCar);
+			
+	}
+
+	public static <T> List<T> filter(List<T> list, Predicate<T> predicate){
+		List<T> filterCar = new ArrayList<>();
+		
+		for (T e : list) {
+			if(predicate.test(e)) {
+				filterCar.add(e );
+			}
+		}
+		
+		return filterCar;
+	}
+}
