0ee3a754ed576059c7ae341a4804ace598901286
kaiquesilvadev <j.kaique1570@gmail.com>
2023-08-31 21:50:20 -0300

    Threads

--- src/ThreadsTest/ThreadsTest01.java
+++ src/ThreadsTest/ThreadsTest01.java
@@ -1,15 +1,32 @@
 package ThreadsTest;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
-public class ThreadsTest01 {
+public class ThreadsTest01{
 
 	public static void main(String[] args) {
-	
-		
- 	}
+		for (int i = 1; i <= 5; i++) {
+			Thread thread = new Thread(new PedidoProcessor(i));
+			thread.start();
+		}
+	}
+}
+
+class PedidoProcessor implements Runnable {
+	private int pedidoId;
+
+	public PedidoProcessor(int pedidoId) {
+		this.pedidoId = pedidoId;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Processando pedido " + pedidoId);
+		// Simulando processamento do pedido
+		try {
+			Thread.sleep(2000); // Simula o tempo de processamento
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			return;
+		}
+		System.out.println("Pedido " + pedidoId + " processado com sucesso.");
+	}
 }
